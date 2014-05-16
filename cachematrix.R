32041fc972768eac9058c27604042a52b4593b1a
florianzbinden <florian.zbinden@sunrise.ch>
2014-05-16 22:31:21 +0200

    1st t for ass2

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,49 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
+
 
 
 ## Write a short comment describing this function
 
+
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m
 }
+
+
+
+
+ss <- makeCacheMatrix()           # initialize
+ss2 <- matrix(c(2,2,3,2), 2, 2)
+ss <- makeCacheMatrix(ss2)
+ss$get                            # get the vector (pas oblig.)
+cacheSolve(ss)                    # calculate the mean
+cacheSolve(ss)                    # when is called back use the cached mean  
+
+
+
