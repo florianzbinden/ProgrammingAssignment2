e4a611fd478f58d4689d2e39d0888185d5d6f164
florianzbinden <florian.zbinden@sunrise.ch>
2014-05-19 21:22:36 +0200

    3rd version

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,10 @@
-##  overall description of what the functions do
 
-
+## Welcome ! Below is my code for the programming assignment 2.
 
 ## the makeCacheMatrix function calculates the inverse of a matrix and caches its value
 
-
-makeCacheMatrix <- function(x = numeric()) {     # argument is an empty numeric matrix (stored in function's environment). This function intiates a "child" environment (while the current R session is the "parent's" environment).
+makeCacheMatrix <- function(x = numeric()) {     # argument is an empty numeric matrix (stored in function's environment). 
+                                                 # This function intiates a "child" environment (while the current R session is the "parent's" environment).
   m <- NULL                                      # internal variable m is initiated and set to NULL
   set <- function(y) {                           # function with y as an argument
     x <<- y                                      # assigns the matrix into x (into parent's environment where the fonction is called)
@@ -20,11 +19,13 @@ makeCacheMatrix <- function(x = numeric()) {     # argument is an empty numeric 
 }
 
 
-## this function gets the value of the inverse of the matrix and if the second matrix submitted has not changed then it returns the value from the cache
+## this function gets the value of the inverse of the matrix and if the second matrix submitted has not changed 
+   # then it returns the value from the cache
 
 cacheSolve <- function(x, ...) {
   m <- x$getsolve()
-  if(!is.null(m)) {                              # if the value of m is not NULL, then it contains a value; therefore it returns the previous value of inverse as this signifies that the matrix is the same like before
+  if(!is.null(m)) {                              # if the value of m is not NULL, then it contains a value; 
+                                                 # therefore it returns the previous value of inverse as this signifies that the matrix is the same like before
     message("getting cached data")               # it returns a message indicating that the value is stored and will be get
     return(m)                                    # and returns the value of the inverse
   }
@@ -39,9 +40,9 @@ cacheSolve <- function(x, ...) {
 
 
 # for testing the code, use the following commands in the console
-ss <- makeCacheMatrix()           # initialize
+ss <- makeCacheMatrix()           # initialize a matrix
 ss2 <- matrix(c(2,2,3,2), 2, 2)
 ss <- makeCacheMatrix(ss2)
-ss$get                            # get the vector (pas oblig.)
-cacheSolve(ss)                    # calculate the mean
-cacheSolve(ss)                    # when is called back use the cached mean  
+ss$get                            # get the matrix (not mandatory)
+cacheSolve(ss)                    # calculate the inverse
+cacheSolve(ss)                    # when is called back use the cached inverse  
